97cd770a8fe9d63a770fc476a775a185469ede9e
Ryan <ryanlarkin0@gmail.com>
2024-08-14 15:55:10 -0700

    social pipeline

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 INSTALLED_APPS = [
     # My apps
     'account.apps.AccountConfig',
+    'django_extensions',
     'social_django',
 
     # Django apps
@@ -138,6 +139,24 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+# SECURITY WARNING
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET')
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.create_profile',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.user_details',
 ]
