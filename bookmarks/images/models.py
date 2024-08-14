bc16ff3c4d29310fb5eb1db326b3958461d6fd54
Ryan <ryanlarkin0@gmail.com>
2024-08-14 16:04:36 -0700

    image save

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -20,3 +21,8 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
