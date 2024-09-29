480e99f7bd06921a787779aa46d80234b3f6e51c
Ryan <ryanlarkin0@gmail.com>
2024-09-28 22:32:51 -0700

    fin

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -100,6 +100,11 @@ DATABASES = {
     }
 }
 
+# SECURITY WARNING
+REDIS_HOST = config('REDIS_HOST')
+REDIS_PORT = config('REDIS_PORT')
+REDIS_DB = config('REDIS_DB')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
